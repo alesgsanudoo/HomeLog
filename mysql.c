cacba3ea82fecb256ff701869e5fe87279f7fdca
alesgsanudoo_ <alejandrosanudo10@gmail.com>
2023-05-13 12:23:59 +0200

    Added more mysql methods, optimized code

--- mysql.c
+++ mysql.c
@@ -14,17 +14,18 @@
 #include <mysql.h>
 #include <unistd.h>
 
-#define USER_EXISTS (0);
-#define CREATED_FILE (0);
-#define CLOSING_CONN (0);
-#define USER_AVAIL (1);
-#define ERROR (-1);
-#define SUCCESS (0);
+#define USER_EXISTS (0)
+#define CREATED_FILE (1)
+#define CLOSING_CONN (0)
+#define USER_AVAIL (1)
+#define ERROR (-1)
+#define SUCCESS (0)
 
 void show_information(MYSQL *con);
 void insert_information(MYSQL *con, char *query);
 void delete_information(MYSQL *con, char *query);
 int check_information(MYSQL *con, char *name);
+int loadFile();
 
 static char host[200] = {0};
 static char user[200] = {0};
@@ -38,92 +39,51 @@ unsigned int flags = 0;
  * The main will create the database and tables in 
  * order to make the program work correctly. 
  */
-
+ 
 int main(int argc, char *argv[]) {
 
-	FILE *data_ptr = NULL;
-	printf("#######################\n");
-	if (access("database.txt", F_OK) != 0) {
-	  data_ptr = fopen("database.txt", "w");
-	  if (data_ptr == NULL) {
-	  	printf("Something went wrong creating the file!\n");
-	  	return ERROR;
-	  }
-	  printf("Creating database file...\n");
-	  printf("Make sure to modify it before running the code again.\n");
-	  fprintf(data_ptr, "host: localhost\n");
-	  fprintf(data_ptr, "user: root\n");
-	  fprintf(data_ptr, "password: 1234\n");
-	  fprintf(data_ptr, "database: test\n");
-	  fprintf(data_ptr, "port: 3306\n");
-	  printf("#######################\n");
-	  return CREATED_FILE;
-	} 
-	else {
-		data_ptr = fopen("database.txt", "r");
-	  if (data_ptr == NULL) {
-	  	printf("Something went wrong reading the file!\n");
-	  	return ERROR;
-	  }
-	  if (fscanf(data_ptr, "%*[^:]: %s\n", host) != 1) {
-	  	printf("Something went wrong reading the file.\n");
-	  	return ERROR;
-	  }
-	  if (fscanf(data_ptr, "%*[^:]: %s\n", user) != 1) {
-	  	printf("Something went wrong reading the file.\n");
-	  	return ERROR;
-	  }
-	  if (fscanf(data_ptr, "%*[^:]: %s\n", pass) != 1) {
-	  	printf("Something went wrong reading the file.\n");
-	  	return ERROR;
-	  }
-	  if (fscanf(data_ptr, "%*[^:]: %s\n", db) != 1) {
-	  	printf("Something went wrong reading the file.\n");
-	  	return ERROR;
-	  }
-	  if (fscanf(data_ptr, "%*[^:]: %d\n", &port) != 1) {
-	  	printf("Something went wrong reading the file.\n");
-	  	return ERROR;
-	  }
+	int mode = loadFile();
+	if ((mode != CREATED_FILE) && (mode != ERROR)) {
     MYSQL* con = mysql_init(NULL);
 	  MYSQL_RES *res;
     MYSQL_ROW row;
 	  printf("Connecting...\n");
-	  int suc = ERROR;
-	  if (!mysql_real_connect(con, host, user, pass, db, port, socket, flags)) {
-	  	printf("Creating database if does not exist...\n");
-  		char query[100] = {0};
-	  	snprintf(query, 100, "CREATE DATABASE IF NOT EXISTS %s", db);
-	  	if (mysql_query(con, query)) {
-      	printf("Error: %s\n", mysql_error(con));
-      	mysql_close(con);
-      	suc = ERROR;
-      	return ERROR;
-    	} else {
-    		suc = SUCCESS;
-    	}
-	  	printf("Creating table if does not exist...\n");
-    	if (mysql_query(con, "CREATE TABLE IF NOT EXISTS information (name VARCHAR(30), password VARCHAR(20))")) {
-      	printf("Error: %s\n", mysql_error(con));
-      	mysql_close(con);
-      	suc = ERROR;
-      	return ERROR;
-    	} else {
-    		suc = SUCCESS;
-    	}
-    	if (suc == -1) {
-		  	printf("Error %s (%d)", mysql_error(con), mysql_errno(con));
-		  	return ERROR;
-		  }
+	  if (!mysql_real_connect(con, host, user, pass, NULL, port, socket, flags)) {
+		  printf("Error Co %s (%d)\n", mysql_error(con), mysql_errno(con));
+      printf("#############\n");
+		  return ERROR;
   	}
+
+  	char query[200] = {0};
+	  snprintf(query, 199, "CREATE DATABASE IF NOT EXISTS %s", db);
+	  if (mysql_query(con, query)) {
+	  	printf("%s\n", db);
+      printf("Error Cre: %s\n", mysql_error(con));
+      mysql_close(con);
+      printf("#############\n");
+      return ERROR;
+    }
+    char query_db[200] = {0};
+    snprintf(query_db, 199, "USE %s", db);
+    if (mysql_query(con, query_db)) {
+    	printf("Error use: %s\n", mysql_error(con));
+    	mysql_close(con);
+      printf("#############\n");
+      return ERROR;
+    }
+
+    if (mysql_query(con, "CREATE TABLE IF NOT EXISTS information (name VARCHAR(30), password VARCHAR(20))")) {
+      printf("Error Table: %s\n", mysql_error(con));
+      mysql_close(con);
+      printf("#############\n");
+      return ERROR;
+    } 
 	  printf("Databse informtion: \n");
 	  printf("Host: %s\n", host);
   	printf("user: %s\n", user);
-	  printf("password: %s\n", pass);
+	  printf("password: (Hide)\n");
   	printf("IP/Port: %s/%d\n", socket,port);
- 
 	  printf("\n");
-
   /*
   printf("Inserting information (Creating user) \n");
   char *name = "alex";
@@ -145,8 +105,70 @@ int main(int argc, char *argv[]) {
     printf("Mysql Server is ready to use, now please use client.c \n");
 	  printf("#############\n");
 	  return CLOSING_CONN;
-	}
+	} 
 } /* main() */
+
+
+/*
+ * This method will ensure the creation/loading of the database 
+ * information file.
+ */
+
+int loadFile() {
+	FILE *data_ptr = NULL;
+	printf("#######################\n");
+	if (access("database.txt", F_OK) != 0) {
+	  data_ptr = fopen("database.txt", "w");
+	  if (data_ptr == NULL) {
+	  	printf("Something went wrong creating the file!\n");
+	    printf("#######################\n");
+	  	return ERROR;
+	  }
+	  printf("Creating database file...\n");
+	  printf("Make sure to modify it before running the code again.\n");
+	  fprintf(data_ptr, "host: localhost\n");
+	  fprintf(data_ptr, "user: root\n");
+	  fprintf(data_ptr, "password: 1234\n");
+	  fprintf(data_ptr, "database: test\n");
+	  fprintf(data_ptr, "port: 3306\n");
+	  printf("#######################\n");
+	  return CREATED_FILE;
+	} 
+	else {
+		data_ptr = fopen("database.txt", "r");
+	  if (data_ptr == NULL) {
+	  	printf("Something went wrong reading the file!\n");
+	  	printf("#######################\n");
+	  	return ERROR;
+	  }
+	  if (fscanf(data_ptr, "%*[^:]: %s\n", host) != 1) {
+	  	printf("Something went wrong reading the file.\n");
+	  	printf("#######################\n");
+	  	return ERROR;
+	  }
+	  if (fscanf(data_ptr, "%*[^:]: %s\n", user) != 1) {
+	  	printf("Something went wrong reading the file.\n");
+	  	printf("#######################\n");
+	  	return ERROR;
+	  }
+	  if (fscanf(data_ptr, "%*[^:]: %s\n", pass) != 1) {
+	  	printf("Something went wrong reading the file.\n");
+	  	printf("#######################\n");
+	  	return ERROR;
+	  }
+	  if (fscanf(data_ptr, "%*[^:]: %s\n", db) != 1) {
+	  	printf("Something went wrong reading the file.\n");
+	  	printf("#######################\n");
+	  	return ERROR;
+	  }
+	  if (fscanf(data_ptr, "%*[^:]: %d\n", &port) != 1) {
+	  	printf("Something went wrong reading the file.\n");
+	  	printf("#######################\n");
+	  	return ERROR;
+	  }
+	  return SUCCESS;
+	}
+}
 
 /*
  * This method will insert the information 
@@ -176,24 +198,32 @@ void show_information(MYSQL *con) {
     printf("Error: %s\n", mysql_error(con));
     mysql_close(con);
     return;
-   }
-   MYSQL_ROW row;
+  }
+  MYSQL_ROW row;
   while ((row = mysql_fetch_row(result))) {
    	printf("%s %s\n", row[0], row[1]);
   } 
   mysql_free_result(result);
+  return;
 } /* show_information() */
 
 /*
  * This method will delete the information of a user.
  */
 
-void delete_information(MYSQL *con, char *query) {
-	return; 
+void delete_information(MYSQL *con, char *user_name) {
+	char query[200] = {0};
+	snprintf(query, 199, "DELETE FROM information WHERE name= '%s'", user_name);
+	if (mysql_query(con, query)) {
+		printf("Error Del: %s\n", mysql_error(con));
+		mysql_close(con);
+	}
+	return;
 } /* delete_information() */
 
 /*
  * This method will check if the user exists. 
+ *
  */
 
 int check_information(MYSQL *con, char *name) {
@@ -205,11 +235,11 @@ int check_information(MYSQL *con, char *name) {
   }
   MYSQL_ROW row;
   while ((row = mysql_fetch_row(result))) {
-   	 if (strcasecmp(row[0], name) == 0) {
-   	 	 mysql_free_result(result);
-   	 	 return USER_EXISTS;
+   	if (strcasecmp(row[0], name) == 0) {
+   	 	mysql_free_result(result);
+   	 	return USER_EXISTS;
    	}
-  } 
+  }
   mysql_free_result(result);
   return USER_AVAIL;
 } /* check_information() */
